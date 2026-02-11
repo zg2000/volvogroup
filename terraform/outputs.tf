15e26395ad469bbb3f9c698f0dfeaa29e626e5b8
zg2000 <zg2000@yeah.net>
2026-02-11 22:20:20 +0100

    Add outputs for resource group and AKS cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,19 @@
 output "webapp_url" {
   value = azurerm_linux_web_app.testwa.default_hostname
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.aks_rg.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
